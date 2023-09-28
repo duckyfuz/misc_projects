744d4c0a3fc50815b91c1165d0179d344b360485
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-09-28 19:11:22 +0800

    Error handling

--- NodeJS/complete-node-bootcamp/4-natours/starter/app.js
+++ NodeJS/complete-node-bootcamp/4-natours/starter/app.js
@@ -50,9 +50,15 @@ app.post('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id/:y?', (req, res) => {
   // /:y? is an OPTIONAL param -> due to ?
   console.log(req.params);
-
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
 
   res.status(200).json({
     status: 'success',
