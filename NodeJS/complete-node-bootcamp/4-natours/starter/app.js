9d32c4716a13d717b38d28d096e4f264625ba776
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-09-28 18:16:50 +0800

    GET and POST

--- NodeJS/complete-node-bootcamp/4-natours/starter/app.js
+++ NodeJS/complete-node-bootcamp/4-natours/starter/app.js
@@ -1,13 +1,34 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: `Hello from the server!`, app: `Natours` }); // 200 Status is the default btw
+app.use(express.json()); // Middleware
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: `Hello from the server!`, app: `Natours` }); // 200 Status is the default btw
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You have sent a post request');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours, // can write this syntax instead of tours: tours
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You have sent a post request');
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
 });
 
 const port = 3000;
